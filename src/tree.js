5bcbf7736c2ad4144f3afc50e7240ebcb71cb28e
lulin <lulin@g7.com.cn>
2022-10-19 22:30:22 +0800

    feat: 树按层遍历

---
+++ src/tree.js
@@ -0,0 +1,51 @@
+// 按层打印二叉树节点
+function printLevel(root) {
+  /**
+     * res 外层
+     * res.push([]) // [] 表示每一层内层
+    */
+   const res = [];
+
+   if (!root) {
+       return res;
+   }
+
+   // queue 表示每一层，从根节点开始
+   const queue = [root];
+   
+   // 节点需要遍历完
+   while(queue.length > 0) {
+       // 先初始化每一层的数组
+       // [[3], []] // 每次都初始化
+       res.push([]);
+
+       const size = queue.length;
+
+       // 每一层放入的元素需要一次遍历, 最多遍历 2 次，如果到最后节点 node.left && node.right => null
+       // queue size 就清空了
+       for (let i = 1; i <= size; i++) {
+           // 第一次循环只有 1 个元素
+           // 从左打印到右
+           const node = queue.shift(); // left, right
+           // val
+           const nodeValue = node.val;
+
+           // res[1 - 1] => 初始化了一个空数组
+           // res[0].push(nodeValue)
+
+           // res[2 - 1].push(nodeLeftValue1, nodeRightValue1)
+           // res[2 - 1].push(nodeLeftValue2, nodeRightValue2)
+           res[res.length - 1].push(nodeValue);
+
+           // 每次都把 node 叶子字节都放进去，然后每次先 shift 出队完成
+           if (node.left) {
+               queue.push(node.left);
+           }
+           if (node.right) {
+               queue.push(node.right);
+           }
+       }
+   }
+
+   return res;
+}
